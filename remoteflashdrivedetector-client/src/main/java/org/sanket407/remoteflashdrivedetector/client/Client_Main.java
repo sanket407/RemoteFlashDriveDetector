299f8aeabc4e5b02522faf0acc3cc1cddef7df1a
sanket <sanket407@gmail.com>
2017-12-28 19:09:34 +0530

    added properties support for client

--- remoteflashdrivedetector-client/src/main/java/org/sanket407/remoteflashdrivedetector/client/Client_Main.java
+++ remoteflashdrivedetector-client/src/main/java/org/sanket407/remoteflashdrivedetector/client/Client_Main.java
@@ -1,7 +1,10 @@
 package org.sanket407.remoteflashdrivedetector.client;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Properties;
 
 import javax.swing.SwingUtilities;
 
@@ -9,9 +12,22 @@ public class Client_Main
 {
     public static void main(String[] args) throws Exception {
 
+        Properties prop = new Properties();
+        try 
+        {
+            InputStream is = Client_Main.class.getResourceAsStream("/settings.properties");
+            prop.load(is);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        final String operatingSystem = prop.getProperty("operating_system");
+        
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Client_Gui gui = new Client_Gui("ubuntu");
+                Client_Gui gui = new Client_Gui(operatingSystem);
             }
         });
     }
